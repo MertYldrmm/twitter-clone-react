b5963a8545ff2cdbe3d31d586d95547bc1fcbdb2
MertYldrmm <mert.yyldrim@gmail.com>
2021-02-09 01:00:28 +0300

    navButtons

--- components/button.js
+++ components/button.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import cn from 'classnames'
 
 import style from './button.module.css'
 
-function Button({ children, ...probs }) {
+function Button({ children, className, ...probs }) {
   return (
-    <button type="button" className={style.button} {...probs}>
+    <button type="button" className={cn(style.button, className)} {...probs}>
       {children}
     </button>
   )
