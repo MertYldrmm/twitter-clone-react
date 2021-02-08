699694ad5202c3e7aed05d6dd05ad177867643e8
MertYldrmm <mert.yyldrim@gmail.com>
2021-02-08 23:01:03 +0300

    components navigation-button & postcss

--- components/button.js
+++ components/button.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 import style from './button.module.css'
 
-function Button({ children }) {
+function Button({ children, ...probs }) {
   return (
-    <button type="button" className={style.button}>
+    <button type="button" className={style.button} {...probs}>
       {children}
     </button>
   )
--- stories/1-Button.stories.js
+++ stories/1-Button.stories.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
 import Button from '../components/button'
+import NavigationButton from '../components/navigation-button'
+import Navigation from '../components/navigation'
 
 export default {
   title: 'Buttons'
 }
 
 export const Normal = () => <Button>SAVE</Button>
+
+export const NavButton = () => <NavigationButton>SAVE</NavigationButton>
+
+export const Nav = () => <Navigation />
