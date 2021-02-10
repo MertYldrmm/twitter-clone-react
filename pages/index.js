892f1fde36629486c57d55b2d08509754171b67f
MertYldrmm <mert.yyldrim@gmail.com>
2021-02-11 02:03:55 +0300

    hooks: useWindowSize

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
 
 import Layout from '../components/layout'
+import useWindowSize from '../hooks/useWindowSize'
 
 function HomePage() {
+  const size = useWindowSize()
+
   return (
     <Layout>
       <div>sidebar</div>
-      <div>main</div>
-      <div>extra</div>
+      <div>{JSON.stringify(size)}</div>
+      {size.width > 1270 && <div>extra</div>}
     </Layout>
   )
 }
