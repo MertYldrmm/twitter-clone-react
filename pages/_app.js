0c9a6eb528b775d58f4561bc389813fa65204cdc
MertYldrmm <mert.yyldrim@gmail.com>
2021-02-14 20:40:44 +0300

    context & theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useEffect, useLayoutEffect, useState } from 'react'
+
+import StoreContext from '../store'
 
 import '../styles/app.css'
 
 export default function MyApp({ Component, pageProbs }) {
-  return <Component {...pageProbs} />
+  const [theme, themeSet] = useState(null)
+
+  useLayoutEffect(() => {
+    const theme = localStorage.getItem('THEME') || 'light'
+    themeSet(theme)
+  }, [])
+
+  const changeTheme = (theme) => {
+    themeSet(theme)
+    localStorage.setItem('THEME', theme)
+  }
+
+  useEffect(() => {
+    const $html = document.querySelector('html')
+    $html.classList.remove('light')
+    $html.classList.remove('dim')
+    $html.classList.remove('dark')
+    $html.classList.add(theme)
+  }, [theme])
+
+  return (
+    <StoreContext.Provider value={{ theme, changeTheme }}>
+      <Component {...pageProbs} />
+    </StoreContext.Provider>
+  )
 }
